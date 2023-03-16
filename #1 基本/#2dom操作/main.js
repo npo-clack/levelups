04ab3d4f02dceec223ef39b2bb68ef36f877b565
RNm-dove <m.rynk.plume@gmail.com>
2023-03-16 09:05:32 +0900

    Add #1基本コンテンツ

---
+++ #1 基本/#2dom操作/main.js
@@ -0,0 +1,31 @@
+console.log("js読み込み start");
+
+const element1 = document.getElementById("element1");
+const element2 = document.getElementById("element2");
+const element3 = document.getElementById("element3");
+const elements = document.getElementsByClassName("elements");
+// getElementsByTagNameはあまり使わない
+// const divs = document.getElementsByTagName("div");
+
+element1.innerText = "innerTextで新しくelement1に文字列を代入";
+element2.innerHTML = "innerHTMLで新しくelement2に文字列を代入";
+element3.innerText = 'appendChildを使えば他のタグを子要素として追加できる';
+
+// appendChild用のタグを生成
+const newChildTag1 = document.createElement('span');
+// 生成したタグに文章追加
+newChildTag1.innerText = '追加要素１';
+// appendChild用のタグを生成
+const newChildTag2 = document.createElement('span');
+// 生成したタグに文章追加
+newChildTag2.innerText = '追加要素2';
+// appendChildで生成したタグを追加
+element3.appendChild(newChildTag1);
+element3.appendChild(newChildTag2);
+
+for(let i = 0; i < elements.length; i++){
+  const element = elements[i];
+  element.innerText = 'クラスで一気にタグを編集できる。'
+}
+
+console.log("js読み込み done");
---
+++ #1 基本/#3イベント/main.js
@@ -0,0 +1,51 @@
+window.onmousemove = function (mouseEvent){
+  // これをコメントイン(//を消して実行可能にする)してどんなイベントが渡されるかみてみよう
+  // console.log(mouseEvent)
+
+  const mouseDisplayTag = document.getElementById("mouseMove");
+  mouseDisplayTag.innerText = `x: ${mouseEvent.clientX}, y: ${mouseEvent.clientY}`;
+}
+
+window.onkeydown = function (keyEvent){
+  // これをコメントイン(//を消して実行可能にする)してどんなイベントが渡されるかみてみよう
+  // console.log(keyEvent)
+
+  const keyDisplayTag = document.getElementById("keyDown");
+  keyDisplayTag.innerText = `key: ${keyEvent.key}, keyCode: ${keyEvent.keyCode}`
+}
+
+window.onclick = function (clickEvent){
+  // これをコメントイン(//を消して実行可能にする)してどんなイベントが渡されるかみてみよう
+  // console.log(clickEvent)
+
+  const clickDisplayTag = document.getElementById("buttonClick");
+  clickDisplayTag.innerText = `clicked! x: ${clickEvent.clientX}, y: ${clickEvent.clientY}`
+}
+
+// 上は、windowというブラウザそのものにイベントリスナーを定義したが、あるdom要素に対して定義することもできる。
+const tomatoArea = document.getElementById('area');
+
+tomatoArea.onmousemove = function (mouseEvent){
+  // これをコメントイン(//を消して実行可能にする)してどんなイベントが渡されるかみてみよう
+  // console.log(mouseEvent)
+
+  const mouseDisplayTag = document.getElementById("tomatoMouseMove");
+  mouseDisplayTag.innerText = `x: ${mouseEvent.clientX}, y: ${mouseEvent.clientY}`;
+}
+
+// NOTE: divタグには通常キーボードがフォーカスしないのでこれは機能しないが、inputタグがあればキーボードがフォーカスするため機能する。
+tomatoArea.onkeydown = function (keyEvent){
+  // これをコメントイン(//を消して実行可能にする)してどんなイベントが渡されるかみてみよう
+  // console.log(keyEvent)
+
+  const keyDisplayTag = document.getElementById("tomatoKeyDown");
+  keyDisplayTag.innerText = `key: ${keyEvent.key}, keyCode: ${keyEvent.keyCode}`
+}
+
+tomatoArea.onclick = function (clickEvent){
+  // これをコメントイン(//を消して実行可能にする)してどんなイベントが渡されるかみてみよう
+  // console.log(clickEvent)
+
+  const clickDisplayTag = document.getElementById("tomatoButtonClick");
+  clickDisplayTag.innerText = `clicked! x: ${clickEvent.clientX}, y: ${clickEvent.clientY}`
+}
